b1ed7307d990a535f1f0fd5d8f18353adbca4536
Cem Fidan <devcfidan@gmail.com>
2023-05-05 14:12:14 +0300

    10th lesson finished

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Product Added";
         public static string ProductNameInvalid = "Product Name is Invalid";
-
+        public static string MaintenanceTime = "Maintenance Time";
+        public static string ProductsListed = "Products Listed";
     }
 }
--- Core/Utilities/Results/Concrete/DataResult.cs
+++ Core/Utilities/Results/Concrete/DataResult.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Utilities.Results.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace Core.Utilities.Results.Concrete
 {
-    public class DataResult
+    public class DataResult<T> : Result, IDataResult<T>
     {
+        public DataResult(T data, bool success, string message) : base(success, message)
+        {
+            Data = data;
+        }
+
+        public DataResult(T data, bool success) : base(success)
+        {
+            Data = data;
+        }
+
+        public T Data { get; }
     }
 }
