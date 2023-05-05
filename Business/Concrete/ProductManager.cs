16da19f214c1b91cbae83ea16c5f15e51391308a
Cem Fidan <devcfidan@gmail.com>
2023-05-06 00:52:29 +0300

    12th lesson part I

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,11 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
 using Core.Utilities.Results.Abstract;
 using Core.Utilities.Results.Concrete;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,10 +26,12 @@ namespace Business.Concrete
 
         public IResult Add(Product product)
         {
-            if(product.ProductName.Length < 2)
+            var context = new ValidationContext<Product>(product);
+            ProductValidator productValidator = new ProductValidator();
+            var result = productValidator.Validate(context);
+            if(!result.IsValid)
             {
-                return new ErrorResult(Messages.ProductNameInvalid);
-
+                throw new ValidationException(result.Errors);
             }
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
